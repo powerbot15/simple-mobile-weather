6187ada774c641c0b1583a63ede41fb8d10e8166
powerbot15 <powerbot15@gmail.com>
2014-08-18 12:26:39 +0300

    first launch

--- Resources/app.js
+++ Resources/app.js
@@ -1,7 +1,7 @@
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 
-var Informer = require('controller/informer'),
+var Informer = require('controller/informer.js'),
 	app = new Informer();
-
+app.getWeather();
 
 
