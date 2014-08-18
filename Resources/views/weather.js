29df2f075bec0df44a23c28918043a66e54d7e34
powerbot15 <powerbot15@gmail.com>
2014-08-18 12:56:44 +0300

    bug fixing

--- Resources/views/weather.js
+++ Resources/views/weather.js
@@ -7,20 +7,25 @@ function CreateWeatherView(weather){
 	}),
 		closeButton = Ti.UI.createButton({
 			width : '98%',
-			height : '20px',
-			backgroundColor:'#8118B3'
+			// height : '20px',
+			backgroundColor:'#8118B3',
+			title: 'CLOSE'
 			
 		}),
 		caption = Ti.UI.createLabel({
 			color:'#000000',
 			text : weather.name
+		}),
+		picture = Ti.UI.createImageView({
+			image:'http://openweathermap.org/img/w/'+ weather.weather[0].icon +'.png'
 		});
 		
 	closeButton.addEventListener('click', function(event){
 		view.remove();
 	});
 	view.add(closeButton);
-	view.add(caption);	
+	view.add(caption);
+	view.add(picture);	
 	return view;
 	
 }
